7b04f8437660dd70bf01de5200d0c1a1c7aa6009
Bee0510 <beheravishal19@gmail.com>
2023-01-06 00:12:54 +0530

    change del button col-pink

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
         fontFamily: 'OpenSans',
-        errorColor: Colors.red,
+        errorColor: Color.fromARGB(255, 246, 196, 192),
       ),
       home: MyHomePage(),
     );
